3b1a4b37c18e89a01a53bf01d0549bd80ed90020
Andrey Zakharov <fuse5@yandex.ru>
2020-12-23 00:42:22 +0100

    publish demo url

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # VManager Demo v0.0.1
+
+https://video-manager.glitch.me/
+
--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require("path");
 const app = express();
 
 const jsonServer = require('json-server')
-const router = jsonServer.router(path.join(__dirname, "../db.json"))
+const router = jsonServer.router(path.join(__dirname, "./db.json"))
 
 app.use('/api', router);
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "./build")));
 app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "../build", "index.html"));
+  response.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 
 const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, () => {
-  console.log("❇️ VManager Demo server is running on port", port);
+  console.log('VManager Demo server is running on port', port);
 });
